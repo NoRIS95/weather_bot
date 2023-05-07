ed8d5ed675646c97ec0b6fd9dca424ce4a31218f
NoRIS95 <ryzhikov95@gmail.com>
2023-05-07 19:00:33 +0300

    third commit

--- main.py
+++ main.py
@@ -3,31 +3,38 @@ import telebot
 from simplejsondb import Database
 import os
 from dotenv import load_dotenv, find_dotenv
-
+import enum
 load_dotenv()
 TG_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = telebot.TeleBot(TG_TOKEN)
-USER_ANSWERS = Database('user_answers.json', default=dict())
+USER_CONDITIONS = Database('user_states.json', default=dict())
+gm = pygismeteo.Gismeteo()
+class StatusDialog(enum.Enum):
+    STATUS_OF_GREEЕTINGS = 1
+    STATUS_OF_ASK_CITY = 2
 
 
 @bot.message_handler(commands=["start"])
 def hello_message(message):
-    mess = f'Привет, {message.from_user.first_name}! :) Для того, чтобы узнать температуру воздуха в текущий момент времени, введите название города на русском:'
-    USER_ANSWERS.data[message.from_user.id] = mess
-    bot.send_message(message.chat.id, USER_ANSWERS.data[message.chat.id])
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! :) Для того, чтобы узнать температуру воздуха в текущий момент времени, введите название города на русском:')
+    USER_CONDITIONS.data[message.from_user.id] = StatusDialog.STATUS_OF_GREEЕTINGS.value
 
 
 @bot.message_handler()
 def temperature(message):
-    gm = pygismeteo.Gismeteo()
-    search_results = gm.search.by_query(message.text)
-    city_id = search_results[0].id
-    current = gm.current.by_id(city_id)
-    answer = f"Температура в городе {message.text}  на текущий момент времени равна {current.temperature.air.c}"
-    USER_ANSWERS.data[message.from_user.id] = answer
-    bot.send_message(message.chat.id, USER_ANSWERS.data[message.from_user.id])
-
+    if USER_CONDITIONS.data[message.from_user.id] == 1:
+        search_results = gm.search.by_query(message.text)
+        city_id = search_results[0].id
+        current_temperature = gm.current.by_id(city_id)
+        bot.send_message(message.chat.id, f"Температура в городе {message.text}  на текущий момент времени равна {current_temperature.temperature.air.c}")
+        USER_CONDITIONS.data[message.from_user.id] = StatusDialog.STATUS_OF_ASK_CITY.value
+    else:
+        search_results = gm.search.by_query(message.text)
+        city_id = search_results[0].id
+        current_temperature = gm.current.by_id(city_id)
+        bot.send_message(message.chat.id, f"Температура в городе {message.text}  на текущий момент времени равна {current_temperature.temperature.air.c}")
+        USER_CONDITIONS.data[message.from_user.id] = StatusDialog.STATUS_OF_ASK_CITY.value
 
 bot.polling(none_stop= True)
 
